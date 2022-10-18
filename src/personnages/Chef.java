c1e534a33480c90107613c88637758f7fcd786e0
khoshnood dorsa <khd3596a@ens.fsi>
2022-10-18 17:38:51 +0200

    Fix TP2

--- src/personnages/Chef.java
+++ src/personnages/Chef.java
@@ -1,35 +1,29 @@
 package personnages;
 
-public class Gaulois {
+public class Chef {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
-
-	public Gaulois(String nom, int force) {
+	private Village village;
+	
+	public Chef(String nom, int force, int effetPotion , Village village) {
 		this.nom = nom;
 		this.force = force;
+		this.effetPotion = effetPotion;
+		this.village = village;
 	}
-
 	public String getNom() {
 		return nom;
 	}
-
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<<" + texte + ">>");
 	}
 
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
+		return "Le chef " + nom + " du village " + village.getNom() + ":";
 	}
-
 	public void frapper(Romain romain) {
 		System.out.println(nom + "envoi un grand coup dans la màchoir de " + romain.getNom());
 		romain.recevoirCoup(force / 3);
 	}
-
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
-
 }
